c0cb127753fcaf5d7f7731938ba7aa79f8bdc0ba
Long Duong <long66704@gmail.com>
2023-02-03 14:35:34 +0700

    refresh the folder

---
+++ ui-components/src/App.js
@@ -0,0 +1,12 @@
+import MenuDropdown from "./components/menu-dropdown/menu-dropdown";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MenuDropdown />
+    </div>
+  );
+}
+
+export default App;
---
+++ ui-components/src/components/menu-dropdown/menu-dropdown.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./menu-dropdown.scss";
+
+const MenuDropdown = () => {
+  return <div className="menu-dropdown-lkj">menu-dropdown</div>;
+};
+
+export default MenuDropdown;
