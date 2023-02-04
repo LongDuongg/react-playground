0aaadf8f82fee4d85c69ad9635e08b3fda78a21d
Long Duong <long66704@gmail.com>
2023-02-04 23:02:29 +0700

    logic button

--- ui-components/src/components/menu-dropdown/menu-dropdown.jsx
+++ ui-components/src/components/menu-dropdown/menu-dropdown.jsx
@@ -1,15 +1,95 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./menu-dropdown.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faAngleUp,
+  faPenToSquare,
+  faClone,
+  faBoxArchive,
+  faArrowUpRightFromSquare,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const MenuDropdown = () => {
+  const [show, setShow] = useState();
+  const menuRef = useRef();
+
+  window.addEventListener("click", (e) => {
+    // if (e.target !== menuRef.current) {
+    //   setShow(false);
+    // }
+    console.log(e.target === menuRef.current);
+  });
+
   return (
     <div className="menu-dropdown-lkj">
-      <button className="options-btn-hjk">
-        Options{" "}
-        <FontAwesomeIcon className="arrow-down-bnm" icon={faArrowDown} />
+      <button
+        className="options-btn-hjk"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Options
+        {show ? (
+          <FontAwesomeIcon className="arrow-down-bnm" icon={faAngleUp} />
+        ) : (
+          <FontAwesomeIcon className="arrow-down-bnm" icon={faAngleDown} />
+        )}
       </button>
+      {show && (
+        <div ref={menuRef} className="options-value-ghf">
+          <ul>
+            <li
+              onClick={() => {
+                setShow(!show);
+              }}
+            >
+              <FontAwesomeIcon className="edit-bnm" icon={faPenToSquare} />
+              Edit
+            </li>
+
+            <li
+              onClick={() => {
+                setShow(!show);
+              }}
+            >
+              <FontAwesomeIcon className="duplicate-aaa" icon={faClone} />
+              Duplicate
+            </li>
+
+            <li
+              onClick={() => {
+                setShow(!show);
+              }}
+            >
+              <FontAwesomeIcon className="archive-bbb" icon={faBoxArchive} />
+              Archive
+            </li>
+
+            <li
+              onClick={() => {
+                setShow(!show);
+              }}
+            >
+              <FontAwesomeIcon
+                className="move-ccc"
+                icon={faArrowUpRightFromSquare}
+              />
+              Move
+            </li>
+
+            <li
+              onClick={() => {
+                setShow(!show);
+              }}
+            >
+              <FontAwesomeIcon className="delete-ddd" icon={faTrash} />
+              Delete
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
