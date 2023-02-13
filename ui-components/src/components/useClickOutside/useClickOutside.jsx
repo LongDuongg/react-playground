7ea97b7431a5163089cc094a9cd19b889b431d9d
Long Duong <long66704@gmail.com>
2023-02-13 22:08:13 +0700

    custom hook: useClickOutside

---
+++ ui-components/src/components/useClickOutside/useClickOutside.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+export const useClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = (e) => {
+      if (!ref?.current.contains(e.target)) {
+        handler();
+      }
+    };
+
+    document.addEventListener("mousedown", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [ref, handler]);
+};
