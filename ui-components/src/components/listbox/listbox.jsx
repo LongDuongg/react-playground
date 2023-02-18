d048deabc1fa7180e0e385fe613966cda95834f2
Long Duong <long66704@gmail.com>
2023-02-18 11:17:32 +0700

    text-ellipsis using scss

--- ui-components/src/components/listbox/listbox.jsx
+++ ui-components/src/components/listbox/listbox.jsx
@@ -10,16 +10,16 @@ export const Listbox = ({ list, getLabel, onChange, isSelected }) => {
 
   const selected = list.find((item) => isSelected(item));
 
-  const truncateTextWithEllipsis = (text, max) => {
-    // console.log(text.length);
+  // const truncateTextWithEllipsis = (text, max) => {
+  // console.log(text.length);
 
-    if (text.length <= max) {
-      return text;
-    } else {
-      return text.substr(0, max - 1) + "...";
-      // console.log(text.substr(0, max - 1) + "...");
-    }
-  };
+  // if (text.length <= max) {
+  // return text;
+  // } else {
+  // return text.substr(0, max - 1) + "...";
+  // console.log(text.substr(0, max - 1) + "...");
+  // }
+  // };
 
   return (
     <div ref={listboxRef} className="listbox-select-asd">
@@ -50,7 +50,8 @@ export const Listbox = ({ list, getLabel, onChange, isSelected }) => {
                   }}
                 >
                   {isSelected(item) && <i className="fa-solid fa-check " />}
-                  {truncateTextWithEllipsis(getLabel(item), 16)}
+                  {/* {truncateTextWithEllipsis(getLabel(item), 16)} */}
+                  {getLabel(item)}
                 </div>
               );
             })}
