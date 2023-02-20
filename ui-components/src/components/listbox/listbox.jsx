ae8a13ea40854bdd11cde261e8f6dc102e548a9a
Long Duong <long66704@gmail.com>
2023-02-20 09:16:15 +0700

    apply dropdown component to listbox

--- ui-components/src/components/listbox/listbox.jsx
+++ ui-components/src/components/listbox/listbox.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
 import { useClickOutside } from "../../hooks/useClickOutside";
+import Dropdown from "../dropdown/dropdown";
 import "./listbox.scss";
 
 export const Listbox = ({
@@ -28,44 +29,50 @@ export const Listbox = ({
 
   return (
     <div ref={listboxRef} className="listbox-select-asd">
-      <div className="toggle">
-        <div
-          onClick={() => {
-            setShow(!show);
-            // console.log(
-            //   truncateTextWithEllipsis("Durward Reynolds Durward Reynolds", 16)
-            // );
-          }}
-          className="options-btn"
-        >
-          {/* {selected && truncateTextWithEllipsis(getLabel(item), 16)} */}
-          {selected ? getLabel(selected) : placeholder}
-          <i className="fa-solid fa-list icon"></i>
-        </div>
-      </div>
+      <Dropdown
+        renderToggle={() => {
+          return (
+            <div
+              onClick={() => {
+                setShow(!show);
+                // console.log(
+                //   truncateTextWithEllipsis("Durward Reynolds Durward Reynolds", 16)
+                // );
+              }}
+              className="options-btn"
+            >
+              {/* {selected && truncateTextWithEllipsis(getLabel(item), 16)} */}
+              {selected ? getLabel(selected) : placeholder}
+              <i className="fa-solid fa-list icon"></i>
+            </div>
+          );
+        }}
+        renderExpand={() => {
+          return (
+            <div className="options-value">
+              {list.map((item, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="option"
+                    onClick={() => {
+                      setShow(!show);
+                      onChange(item);
+                    }}
+                  >
+                    {isSelected(item) && <i className="fa-solid fa-check " />}
+                    {/* {truncateTextWithEllipsis(getLabel(item), 16)} */}
+                    {getLabel(item)}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        }}
+        showOptions={show}
+      />
 
-      {show && (
-        <div className="expand">
-          <div className="options-value">
-            {list.map((item, i) => {
-              return (
-                <div
-                  key={i}
-                  className="option"
-                  onClick={() => {
-                    setShow(!show);
-                    onChange(item);
-                  }}
-                >
-                  {isSelected(item) && <i className="fa-solid fa-check " />}
-                  {/* {truncateTextWithEllipsis(getLabel(item), 16)} */}
-                  {getLabel(item)}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      )}
+      {show && <div className="expand"></div>}
     </div>
   );
 };
