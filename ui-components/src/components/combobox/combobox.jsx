a2e50c07defaeab13a060cdd029ad36fe195fef2
Long Duong <long66704@gmail.com>
2023-02-25 10:32:10 +0700

    search function

--- ui-components/src/components/combobox/combobox.jsx
+++ ui-components/src/components/combobox/combobox.jsx
@@ -7,14 +7,20 @@ export const Combobox = ({
   getLabel,
   onChange,
   isSelected,
-  placeholder,
+  placeholder = "Choose or search options...",
 }) => {
-  const inputRef = useRef();
   const selected = list.find((item) => isSelected(item));
+  const inputRef = useRef();
   const [state, setState] = useState({
-    text: "",
+    searchText: "",
+    selected: "",
   });
-
+  const comboboxList =
+    state.searchText === null
+      ? list
+      : list.filter((item) =>
+          item.name.toLowerCase().includes(state.searchText.toLowerCase())
+        );
   return (
     <Dropdown
       className="combobox-b22"
@@ -24,9 +30,16 @@ export const Combobox = ({
             <input
               ref={inputRef}
               type="text"
-              value={state?.text}
-              onChange={() => {}}
-              placeholder=""
+              value={state?.searchText || state?.selected}
+              onChange={(e) => {
+                setState({
+                  ...state,
+                  searchText: e.target.value,
+                  selected: "",
+                });
+                showExpand(!expanding);
+              }}
+              placeholder={placeholder}
             />
             <i
               className="fa-solid fa-list icon"
@@ -41,13 +54,13 @@ export const Combobox = ({
       renderExpand={({ close }) => {
         return (
           <div className="combobox-list">
-            {list.map((item, index) => {
+            {comboboxList.map((item, index) => {
               return (
                 <div
                   onClick={() => {
                     close();
                     onChange(item);
-                    setState({ ...state, text: getLabel(item) });
+                    setState({ ...state, selected: getLabel(item) });
                   }}
                   key={index}
                   className="option"
