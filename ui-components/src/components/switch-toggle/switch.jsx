c4fd275df8c231917f7552075cc00c0eddf28829
Long Duong <long66704@gmail.com>
2023-03-09 01:01:38 +0700

    making animation for toggle but not work

--- ui-components/src/components/switch-toggle/switch.jsx
+++ ui-components/src/components/switch-toggle/switch.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./switch.scss";
 
 export const Switch = () => {
+  const [check, setCheck] = useState();
+
   return (
     <div className="switch-toggle-abc">
-      <button className="switch-btn">
-        <span className="white-circle">Se</span>
+      <button
+        className="switch-btn"
+        style={
+          check
+            ? { backgroundColor: "#6b7280" }
+            : { backgroundColor: "#9197a3" }
+        }
+        onClick={() => {
+          setCheck(!check);
+        }}
+      >
+        <span
+          className="white-circle"
+          style={
+            check
+              ? {
+                  left: "63px",
+                  // transitionProperty: "transform",
+                  // transitionDuration: "1s",
+                }
+              : {
+                  right: "63px",
+                  // transitionProperty: "transform",
+                  // transitionDuration: "1s",
+                }
+          }
+        >
+          Se
+        </span>
       </button>
     </div>
   );
