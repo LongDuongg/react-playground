0b24b1d9af43668d2442d77f786bbb0f0c923d5c
Long Duong <long66704@gmail.com>
2023-03-14 11:53:19 +0700

    making expand

--- ui-components/src/components/disclosure/disclosure.jsx
+++ ui-components/src/components/disclosure/disclosure.jsx
@@ -16,6 +16,18 @@ export const Disclosure = () => {
           90 days and we'll refund you in full, no questions asked.
         </div>
       </div>
+      {/* <div className="disclosure-group">
+        <div className="toggle">
+          What is your refund policy ?
+          <div className="icon">
+            <i className="fa-solid fa-angle-up"></i>
+          </div>
+        </div>
+        <div className="expand">
+          If you're unhappy with your purchase for any reason, email us within
+          90 days and we'll refund you in full, no questions asked.
+        </div>
+      </div> */}
     </div>
   );
 };
