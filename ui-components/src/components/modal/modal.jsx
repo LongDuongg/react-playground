8015049ec5c263535157be09fe1f74358fbc9ca6
Long Duong <long66704@gmail.com>
2023-03-24 21:46:39 +0700

    fix some details

--- ui-components/src/components/modal/modal.jsx
+++ ui-components/src/components/modal/modal.jsx
@@ -1,22 +1,17 @@
 import React, { useRef } from "react";
 import { useClickOutside } from "../../hooks/useClickOutside";
+import clns from "classnames";
 import "./modal.scss";
 
-export const Modal = ({ isOpen, onClose, title, content, btnText }) => {
+export const Modal = ({ isOpen, onClose, className, renderModal }) => {
   const modalRef = useRef();
   useClickOutside({ ref: modalRef, handler: () => onClose() });
   return (
     <>
       {isOpen && (
-        <div className="modal-overlay">
-          <div ref={modalRef} className="modal-container">
-            <div className="title">
-              <div>{title}</div>
-            </div>
-            <div className="content">{content}</div>
-            <button onClick={() => onClose()} className="btn">
-              {btnText}
-            </button>
+        <div className="modal-overlay-las">
+          <div ref={modalRef} className={clns("modal-container", className)}>
+            {renderModal({ close: () => onClose() })}
           </div>
         </div>
       )}
--- ui-components/src/pages/test-modal.jsx
+++ ui-components/src/pages/test-modal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import "./test-modal.scss";
 import { Modal } from "../components/modal/modal";
+import "./test-modal.scss";
 
 export const Testmodal = () => {
   const [show, setShow] = useState();
@@ -13,11 +13,23 @@ export const Testmodal = () => {
       <Modal
         isOpen={show}
         onClose={() => setShow(false)}
-        title={"Payment successful"}
-        content={
-          "Your payment has been successfully submitted. We have sent you an email with all of the details of your order."
-        }
-        btnText={"Got it, thanks !"}
+        className={"payment-abc"}
+        renderModal={({ close }) => {
+          return (
+            <>
+              <div className="title">
+                <div>Payment successful</div>
+              </div>
+              <div className="content">
+                Your payment has been successfully submitted. We have sent you
+                an email with all of the details of your order.
+              </div>
+              <button onClick={() => close()} className="btn">
+                Got it, thanks !
+              </button>
+            </>
+          );
+        }}
       />
     </div>
   );
