446bc4e5d8f0a5bd865476cee73f6a1d0ddcf017
LongDuongg <long2622001@gmail.com>
2025-02-07 21:09:53 +0700

    setup everything to zero

--- ui-components-new/src/pages/Sample.tsx
+++ ui-components-new/src/pages/Sample.tsx
@@ -2,7 +2,7 @@ import "./Sample.css";
 
 export default function Sample() {
   return (
-    <div className="justify-center...">
+    <div className="flex justify-center">
       <h1 className="text-blue-500">
         Sample
       </h1>
