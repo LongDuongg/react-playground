81c0d6baeed04fa4bfde2fae5549372fc464a3f3
LongDuongg <long2622001@gmail.com>
2025-02-07 20:57:23 +0700

    do silly things

---
+++ ui-components-new/src/pages/Sample.tsx
@@ -0,0 +1,11 @@
+import "./Sample.css";
+
+export default function Sample() {
+  return (
+    <div className="justify-center...">
+      <h1 className="text-blue-500">
+        Sample
+      </h1>
+    </div>
+  );
+}
