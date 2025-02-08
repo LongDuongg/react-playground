0eb6a2598e313ba434749090dc3e46e4fe49272d
LongDuongg <long2622001@gmail.com>
2025-02-08 20:34:00 +0700

    apply dropdown template to menu dropdown

--- ui-components-new/src/components/Menu-Dropdown/Menu-Dropdown.tsx
+++ ui-components-new/src/components/Menu-Dropdown/Menu-Dropdown.tsx
@@ -1,5 +1,4 @@
-import { useState, useRef, FC } from "react";
-import { useClickOutside } from "../../hook/useClickOutside";
+import Dropdown from "../dropdown/dropdown";
 
 type Props = {
   title: string;
@@ -11,51 +10,47 @@ type Props = {
 
 // const MenuDropdown:FC<Props> = ({ title, options }) => {
 export default function MenuDropdown({ title, options }: Props) {
-  const [isOpen, setIsOpen] = useState(false);
-  const ref = useRef(null);
-  useClickOutside({
-    ref,
-    handler: () => setIsOpen(false),
-  });
-
-  const expandWidth = 300;
-
   return (
-    <div ref={ref} className="menu-dropdown w-full relative">
-      <button
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-        className="w-full bg-indigo-500 text-stone-50 p-2 rounded font-bold hover:bg-indigo-600 cursor-pointer"
-      >
-        {title}
-        {isOpen ? (
-          <i className="fa-solid fa-angle-up ml-3"></i>
-        ) : (
-          <i className="fa-solid fa-angle-down ml-3"></i>
-        )}
-      </button>
-      {isOpen && (
-        <div
-          style={{ width: expandWidth }}
-          className="bg-blue-400 rounded p-2 absolute right-0 top-[50px]"
-        >
-          {options.map((option, index) => {
-            return (
-              <div
-                key={index}
-                className="text-stone-50 font-bold hover:bg-indigo-600 hover:rounded-[.5rem] cursor-pointer p-2"
-              >
-                <i
-                  className={`fa-solid ${option.iconClass} text-indigo-500 mr-[0.7rem]`}
-                ></i>
-                <span>{option.text}</span>
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </div>
+    <Dropdown
+      className="w-full relative"
+      renderToggle={({ showExpand, isOpen }: any) => {
+        return (
+          <button
+            onClick={() => {
+              showExpand(!isOpen);
+            }}
+            className="w-full bg-indigo-500 text-stone-50 p-2 rounded font-bold hover:bg-indigo-600 cursor-pointer"
+          >
+            {title}
+            {isOpen ? (
+              <i className="fa-solid fa-angle-up ml-3"></i>
+            ) : (
+              <i className="fa-solid fa-angle-down ml-3"></i>
+            )}
+          </button>
+        );
+      }}
+      renderExpand={({ close }: any) => {
+        return (
+          <>
+            {options.map((option, index) => {
+              return (
+                <div
+                  key={index}
+                  className="text-stone-50 font-bold hover:bg-indigo-600 hover:rounded-[.5rem] cursor-pointer p-2"
+                >
+                  <i
+                    className={`fa-solid ${option.iconClass} text-indigo-500 mr-[0.7rem]`}
+                  ></i>
+                  <span>{option.text}</span>
+                </div>
+              );
+            })}
+          </>
+        );
+      }}
+      expandWidth={300}
+    />
   );
 }
 
