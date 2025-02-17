c2981d72930f6186a9f9041da1aae2c27deb1625
LongDuongg <long2622001@gmail.com>
2025-02-17 20:52:02 +0700

    apply listbox component

--- ui-components-new/src/pages/Sample.tsx
+++ ui-components-new/src/pages/Sample.tsx
@@ -1,12 +1,15 @@
 import "./Sample.css";
-import MenuDropdown from "../components/menu-dropdown/menu-dropdown";
-import { options } from "../mockdata/mockdata";
+// import MenuDropdown from "../components/menu-dropdown/menu-dropdown";
+import Listbox from "../components/listbox/listbox";
+import { options, list } from "../mockdata/mockdata";
+
 
 export default function Sample() {
   return (
     <div className="">
       <div className="w-[150px] ml-auto mr-auto">
-        <MenuDropdown title="Options" options={options} />
+        {/* <MenuDropdown title="Options" options={options} /> */}
+        <Listbox list={list} />
       </div>
     </div>
   );
