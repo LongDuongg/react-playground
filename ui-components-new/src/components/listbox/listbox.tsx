7bf7e9b8ee2f3dc0f95f03a63c5df8c4271fb526
LongDuongg <long2622001@gmail.com>
2025-02-22 09:12:35 +0700

    fix style toggle for each side balance and create distance between text and icon check in menu

--- ui-components-new/src/components/listbox/listbox.tsx
+++ ui-components-new/src/components/listbox/listbox.tsx
@@ -32,10 +32,10 @@ export default function Listbox({
             onClick={() => {
               showExpand(!isOpen);
             }}
-            className="w-full bg-indigo-500 text-stone-50 p-2 rounded font-bold hover:bg-indigo-600 cursor-pointer"
+            className="w-full bg-indigo-500 text-stone-50 p-2 rounded font-bold hover:bg-indigo-600 cursor-pointer text-left pl-5"
           >
             {selected ? getLabel(selected) : ""}
-            <i className="fa-solid fa-list icon"></i>
+            <i className="fa-solid fa-list relative left-40"></i>
           </button>
         );
       }}
@@ -46,16 +46,16 @@ export default function Listbox({
               return (
                 <div
                   key={index}
-                  className="text-stone-50 font-bold hover:bg-indigo-600 hover:rounded cursor-pointer p-2"
+                  className="text-stone-50 font-bold hover:bg-indigo-600 hover:rounded cursor-pointer p-2 relative"
                   onClick={() => {
                     onChange(item);
                     close();
                   }}
                 >
                   {isSelected(item) && (
-                    <i className="fa-solid fa-check icon"></i>
+                    <i className="fa-solid fa-check absolute left-2.5 top-3"></i>
                   )}
-                  <span>{item.text}</span>
+                  <span className="pl-7">{item.text}</span>
                 </div>
               );
             })}
