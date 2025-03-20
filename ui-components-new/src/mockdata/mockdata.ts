e2e9c7aa8b012796f45d7340563483f94128b89d
LongDuongg <long2622001@gmail.com>
2025-03-20 18:44:20 +0700

    adjust data in categories

--- ui-components-new/src/mockdata/mockdata.ts
+++ ui-components-new/src/mockdata/mockdata.ts
@@ -77,17 +77,16 @@ export const plans = [
 
 export const categories = [
   {
+    id: 1,
     name: "Recent",
     details: [
       {
-        id: 1,
         title: "Does drinking coffee make you smarter?",
         date: "5h ago",
         commentCount: 5,
         shareCount: 2,
       },
       {
-        id: 2,
         title: "So you've bought coffee... now what?",
         date: "2h ago",
         commentCount: 3,
@@ -96,17 +95,16 @@ export const categories = [
     ],
   },
   {
+    id: 2,
     name: "Popular",
     details: [
       {
-        id: 1,
         title: "Is tech making coffee better or worse?",
         date: "Jan 7",
         commentCount: 29,
         shareCount: 16,
       },
       {
-        id: 2,
         title: "The most innovative things happening in coffee",
         date: "Mar 19",
         commentCount: 24,
@@ -115,17 +113,16 @@ export const categories = [
     ],
   },
   {
+    id: 3,
     name: "Trending",
     details: [
       {
-        id: 1,
         title: "Ask Me Anything: 10 answers to your questions about coffee",
         date: "2d ago",
         commentCount: 9,
         shareCount: 5,
       },
       {
-        id: 2,
         title: "The worst advice we've ever heard about coffee",
         date: "4d ago",
         commentCount: 1,
