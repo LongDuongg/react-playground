8f320fd09ee3fc8043a1a99a15118927ff8426e0
LongDuongg <long2622001@gmail.com>
2025-06-15 21:04:53 +0700

    provider

---
+++ real-world-app/src/provider/provider.tsx
@@ -0,0 +1,23 @@
+import { useState, createContext } from "react";
+import { createGuestApis } from "../apis/guest-apis,js";
+import { createApis } from "../apis/apis,js";
+import { Auth } from "../loaders/auth.js";
+
+const context = createContext({});
+
+export const provider = ({ children }: any) => {
+    const guestApis = createGuestApis();
+
+    const auth = Auth({ guestApis });
+
+    const apis = createApis({
+        onUnauthen: (message: string) => {
+            // auth.logout();
+            // navigate("/login");
+            window.alert(message);
+        },
+        token: auth.user?.token,
+    });
+
+    return <context.Provider value={{ auth, guestApis, apis }}>{children}</context.Provider>;
+};
