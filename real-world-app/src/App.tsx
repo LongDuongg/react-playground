13965a06f9a01777f47100cc0cb7d4bedade9dd6
LongDuongg <long2622001@gmail.com>
2025-06-17 15:28:30 +0700

    wrap query client and provider

--- real-world-app/src/App.tsx
+++ real-world-app/src/App.tsx
@@ -1,3 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "./provider/provider";
+
 export const App = () => {
-  return <div className="App">abc</div>;
+    return (
+        <QueryClientProvider client={new QueryClient()}>
+            <Provider>
+                <HashRouter>
+                    <Routes>
+                        <Route path="/" element={<h1>Home</h1>} />
+                        <Route path="/" element={<h1>Home</h1>} />
+                    </Routes>
+                </HashRouter>
+            </Provider>
+        </QueryClientProvider>
+    );
 };
