3998a84d4d2563b76f106998b213224eb7a20f93
LongDuongg <long2622001@gmail.com>
2025-06-19 17:17:58 +0700

    fix bug

--- real-world-app/src/routes/home/tag-panel.tsx
+++ real-world-app/src/routes/home/tag-panel.tsx
@@ -1,15 +1,25 @@
-export const TagPanel = ({ title, selectedTag }) => {
+import { useQuery } from "@tanstack/react-query";
+import { consumeContext } from "../../provider/provider";
+
+export const TagPanel = ({ title, onSelect }: { title: string; onSelect: (v: string) => void }) => {
+    const { apis } = consumeContext();
+
+    const tags = useQuery({
+        queryKey: ["tags"],
+        queryFn: () => apis.tag.getTags(),
+    });
+
     return (
         <div className="sidebar">
             <p>{title}</p>
             <div className="tag-list">
-                {tags?.tags.slice(0, 89).map((tag, i) => (
+                {tags.data.tags.slice(0, 89).map((tag: any, i: any) => (
                     <div
                         style={{ cursor: "pointer" }}
                         key={i}
                         className="tag-pill tag-default"
                         onClick={() => {
-                            selectedTag.onChange(tag);
+                            onSelect(tag);
                         }}
                     >
                         {tag}
