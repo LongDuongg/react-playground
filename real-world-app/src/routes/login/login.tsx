4f5a183c70f6dc1fb01a4bab6580bb81b6faf2a3
LongDuongg <long2622001@gmail.com>
2025-06-19 16:51:52 +0700

    add Layout

--- real-world-app/src/routes/login/login.tsx
+++ real-world-app/src/routes/login/login.tsx
@@ -2,6 +2,7 @@ import { Form, Input, Button } from "antd";
 import { useMutation } from "@tanstack/react-query";
 
 import { consumeContext } from "../../provider/provider.tsx";
+import { Layout } from "../layout/layout.tsx";
 
 type Fields = {
     email: string;
@@ -20,56 +21,62 @@ export const Login = () => {
     }
 
     return (
-        <div className="auth-page">
-            <div className="container page">
-                <div className="row">
-                    <div className="col-md-6 offset-md-3 col-xs-12">
-                        <h1 className="text-xs-center">Sign in</h1>
-                        <p className="text-xs-center">
-                            <a href="/register">Need an account?</a>
-                        </p>
+        <Layout>
+            <div className="auth-page">
+                <div className="container page">
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3 col-xs-12">
+                            <h1 className="text-xs-center">Sign in</h1>
+                            <p className="text-xs-center">
+                                <a href="/register">Need an account?</a>
+                            </p>
 
-                        {mutation.data?.errors && (
-                            <ul className="error-messages">
-                                <li>{mutation.data.errors.body}</li>
-                            </ul>
-                        )}
+                            {mutation.data?.errors && (
+                                <ul className="error-messages">
+                                    <li>{mutation.data.errors.body}</li>
+                                </ul>
+                            )}
 
-                        <Form
-                            name="signup-form"
-                            labelCol={{ span: 8 }}
-                            wrapperCol={{ span: 16 }}
-                            style={{ maxWidth: 600 }}
-                            initialValues={{ remember: true }}
-                            onFinish={(value: any) => {
-                                mutation.mutate(value);
-                            }}
-                            // onFinishFailed={onFinishFailed}
-                            autoComplete="off"
-                        >
-                            <Form.Item<Fields>
-                                name="email"
-                                rules={[{ required: true, message: "Please input your email!" }]}
+                            <Form
+                                name="signup-form"
+                                labelCol={{ span: 8 }}
+                                wrapperCol={{ span: 16 }}
+                                style={{ maxWidth: 600 }}
+                                initialValues={{ remember: true }}
+                                onFinish={(value: any) => {
+                                    mutation.mutate(value);
+                                }}
+                                // onFinishFailed={onFinishFailed}
+                                autoComplete="off"
                             >
-                                <Input placeholder="Email" />
-                            </Form.Item>
+                                <Form.Item<Fields>
+                                    name="email"
+                                    rules={[
+                                        { required: true, message: "Please input your email!" },
+                                    ]}
+                                >
+                                    <Input placeholder="Email" />
+                                </Form.Item>
 
-                            <Form.Item<Fields>
-                                name="password"
-                                rules={[{ required: true, message: "Please input your password!" }]}
-                            >
-                                <Input.Password placeholder="Password" />
-                            </Form.Item>
+                                <Form.Item<Fields>
+                                    name="password"
+                                    rules={[
+                                        { required: true, message: "Please input your password!" },
+                                    ]}
+                                >
+                                    <Input.Password placeholder="Password" />
+                                </Form.Item>
 
-                            <Form.Item label={null}>
-                                <Button type="primary" htmlType="submit">
-                                    {mutation.isPending ? "Loading..." : "Sign in"}
-                                </Button>
-                            </Form.Item>
-                        </Form>
+                                <Form.Item label={null}>
+                                    <Button type="primary" htmlType="submit">
+                                        {mutation.isPending ? "Loading..." : "Sign in"}
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Layout>
     );
 };
--- real-world-app/src/routes/signup/signup.tsx
+++ real-world-app/src/routes/signup/signup.tsx
@@ -2,6 +2,7 @@ import { Form, Input, Button } from "antd";
 import { useMutation } from "@tanstack/react-query";
 
 import { consumeContext } from "../../provider/provider.tsx";
+import { Layout } from "../layout/layout.tsx";
 
 type Fields = {
     username: string;
@@ -21,63 +22,71 @@ export const Signup = () => {
     }
 
     return (
-        <div className="auth-page">
-            <div className="container page">
-                <div className="row">
-                    <div className="col-md-6 offset-md-3 col-xs-12">
-                        <h1 className="text-xs-center">Sign up</h1>
-                        <p className="text-xs-center">
-                            <a href="/login">Have an account?</a>
-                        </p>
+        <Layout>
+            <div className="auth-page">
+                <div className="container page">
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3 col-xs-12">
+                            <h1 className="text-xs-center">Sign up</h1>
+                            <p className="text-xs-center">
+                                <a href="/login">Have an account?</a>
+                            </p>
 
-                        {mutation.data?.errors && (
-                            <ul className="error-messages">
-                                <li>{mutation.data.errors.body}</li>
-                            </ul>
-                        )}
+                            {mutation.data?.errors && (
+                                <ul className="error-messages">
+                                    <li>{mutation.data.errors.body}</li>
+                                </ul>
+                            )}
 
-                        <Form
-                            name="signup-form"
-                            labelCol={{ span: 8 }}
-                            wrapperCol={{ span: 16 }}
-                            style={{ maxWidth: 600 }}
-                            initialValues={{ remember: true }}
-                            onFinish={(value: any) => {
-                                mutation.mutate(value);
-                            }}
-                            // onFinishFailed={onFinishFailed}
-                            autoComplete="off"
-                        >
-                            <Form.Item<Fields>
-                                name="username"
-                                rules={[{ required: true, message: "Please input your username!" }]}
+                            <Form
+                                name="signup-form"
+                                labelCol={{ span: 8 }}
+                                wrapperCol={{ span: 16 }}
+                                style={{ maxWidth: 600 }}
+                                initialValues={{ remember: true }}
+                                onFinish={(value: any) => {
+                                    mutation.mutate(value);
+                                }}
+                                // onFinishFailed={onFinishFailed}
+                                autoComplete="off"
                             >
-                                <Input placeholder="Username" />
-                            </Form.Item>
+                                <Form.Item<Fields>
+                                    name="username"
+                                    rules={[
+                                        { required: true, message: "Please input your username!" },
+                                    ]}
+                                >
+                                    <Input placeholder="Username" />
+                                </Form.Item>
 
-                            <Form.Item<Fields>
-                                name="email"
-                                rules={[{ required: true, message: "Please input your email!" }]}
-                            >
-                                <Input placeholder="Email" />
-                            </Form.Item>
+                                <Form.Item<Fields>
+                                    name="email"
+                                    rules={[
+                                        { required: true, message: "Please input your email!" },
+                                    ]}
+                                >
+                                    <Input placeholder="Email" />
+                                </Form.Item>
 
-                            <Form.Item<Fields>
-                                name="password"
-                                rules={[{ required: true, message: "Please input your password!" }]}
-                            >
-                                <Input.Password placeholder="Password" />
-                            </Form.Item>
+                                <Form.Item<Fields>
+                                    name="password"
+                                    rules={[
+                                        { required: true, message: "Please input your password!" },
+                                    ]}
+                                >
+                                    <Input.Password placeholder="Password" />
+                                </Form.Item>
 
-                            <Form.Item label={null}>
-                                <Button type="primary" htmlType="submit">
-                                    {mutation.isPending ? "Loading..." : "Sign up"}
-                                </Button>
-                            </Form.Item>
-                        </Form>
+                                <Form.Item label={null}>
+                                    <Button type="primary" htmlType="submit">
+                                        {mutation.isPending ? "Loading..." : "Sign up"}
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Layout>
     );
 };
