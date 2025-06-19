ea826fd561a354f6394c003d1149c004353e469d
LongDuongg <long2622001@gmail.com>
2025-06-19 22:57:58 +0700

    apply context

--- real-world-app/src/App.tsx
+++ real-world-app/src/App.tsx
@@ -1,22 +1,26 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {HashRouter, Route, Routes} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-import { Provider } from "./provider/provider";
-import { Login } from "./routes/login/login.tsx";
-import { Signup } from "./routes/signup/signup.tsx";
+import {Login} from "./routes/login/login.tsx";
+import {Signup} from "./routes/signup/signup.tsx";
+import {Home} from "./routes/home/home.tsx";
+import {GuestApiContextProvider} from "./context/apis.tsx";
+import {AuthContextProvider} from "./context/auth.tsx";
 
 export const App = () => {
-    return (
-        <QueryClientProvider client={new QueryClient()}>
-            <Provider>
-                <HashRouter>
-                    <Routes>
-                        <Route path="/" element={<h1>Home</h1>} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Signup />} />
-                    </Routes>
-                </HashRouter>
-            </Provider>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <GuestApiContextProvider>
+        <AuthContextProvider>
+          <HashRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Signup />} />
+            </Routes>
+          </HashRouter>
+        </AuthContextProvider>
+      </GuestApiContextProvider>
+    </QueryClientProvider>
+  );
 };
--- real-world-app/src/apis/guest-apis.ts
+++ real-world-app/src/apis/guest-apis.ts
@@ -1,11 +1,11 @@
 const API_HOST = "https://conduit-realworld-example-app.fly.dev";
 
 export const createGuestApis = () => {
-    const fetcher = createFetcher();
-    return {
-        user: {
-            // prettier-ignore
-            getUser: async ({ authToken } : { authToken: string | undefined}) => {
+  const fetcher = createFetcher();
+  return {
+    user: {
+      // prettier-ignore
+      getUser: async ({ authToken } : { authToken: string | undefined}) => {
                 const res = await fetcher.get("/user", { authToken });
                 if (res.errors) {
                     return null;
@@ -13,16 +13,16 @@ export const createGuestApis = () => {
                 return res;
             },
 
-            // prettier-ignore
-            login: ({ email, password }: { email: string; password: string }) => fetcher.post("/users/login", {
+      // prettier-ignore
+      login: ({ email, password }: { email: string; password: string }) => fetcher.post("/users/login", {
                 user: {
                     email,
                     password,
                 },
             }),
 
-            // prettier-ignore
-            signUp: async ({ username, email, password }: { username: string; email: string; password: string }) => {
+      // prettier-ignore
+      signUp: async ({ username, email, password }: { username: string; email: string; password: string }) => {
                 return fetcher.post("/users", {
                     user: {
                         username,
@@ -31,38 +31,38 @@ export const createGuestApis = () => {
                     },
                 });
             },
-        },
-    };
+    },
+  };
 };
 
 const createFetcher = () => {
-    const makeRequest = (method: string, noPayload: boolean) => {
-        return async (url: string, payload: any, options: any) => {
-            if (noPayload) {
-                options = payload;
-                payload = null;
-            }
+  const makeRequest = (method: string, noPayload?: boolean) => {
+    return async (url: string, payload: any, options?: any) => {
+      if (noPayload) {
+        options = payload;
+        payload = null;
+      }
 
-            const headers = new Headers();
-            headers.append("Content-Type", "application/json");
+      const headers = new Headers();
+      headers.append("Content-Type", "application/json");
 
-            if (options?.authToken) {
-                headers.append("Authorization", `Token ${options.authToken}`);
-            }
+      if (options?.authToken) {
+        headers.append("Authorization", `Token ${options.authToken}`);
+      }
 
-            const res = await fetch(`${API_HOST}/api${url}`, {
-                method,
-                headers,
-                body: payload ? JSON.stringify(payload) : null,
-            });
+      const res = await fetch(`${API_HOST}/api${url}`, {
+        method,
+        headers,
+        body: payload ? JSON.stringify(payload) : null,
+      });
 
-            return await res.json();
-        };
+      return await res.json();
     };
-    return {
-        get: makeRequest("GET", true),
-        post: makeRequest("POST"),
-        put: makeRequest("PUT"),
-        delete: makeRequest("DELETE", true),
-    };
+  };
+  return {
+    get: makeRequest("GET", true),
+    post: makeRequest("POST"),
+    put: makeRequest("PUT"),
+    delete: makeRequest("DELETE", true),
+  };
 };
