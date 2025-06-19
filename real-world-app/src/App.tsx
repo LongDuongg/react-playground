36fe283644938041c7ff62f03035c4d17186f864
LongDuongg <long2622001@gmail.com>
2025-06-19 09:26:56 +0700

    signup

--- real-world-app/src/App.tsx
+++ real-world-app/src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Provider } from "./provider/provider";
 import { Login } from "./routes/login/login.tsx";
+import { Signup } from "./routes/signup/signup.tsx";
 
 export const App = () => {
     return (
@@ -12,6 +13,7 @@ export const App = () => {
                     <Routes>
                         <Route path="/" element={<h1>Home</h1>} />
                         <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Signup />} />
                     </Routes>
                 </HashRouter>
             </Provider>
