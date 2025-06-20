56fccdfa130f50622350aafa4b27015a34b1e2db
LongDuongg <long2622001@gmail.com>
2025-06-20 10:48:43 +0700

    pass user token after login

--- real-world-app/src/context/apis.tsx
+++ real-world-app/src/context/apis.tsx
@@ -1,20 +1,22 @@
-import { createContext, useContext } from "react";
-import { createGuestApis } from "../apis/guest-apis.ts";
-import { createApis } from "../apis/apis.ts";
+import {createContext, useContext} from "react";
+import {createGuestApis} from "../apis/guest-apis.ts";
+import {createApis} from "../apis/apis.ts";
+import {useAuth} from "./auth.tsx";
 
 const ApiContext = createContext<any>(null);
 
-export const ApiContextProvider = ({ children }: any) => {
+export const ApiContextProvider = ({children}: any) => {
+  const {user} = useAuth();
   const apis = createApis({
     onUnauthen: (message: string) => {
       // auth.logout();
       // navigate("/login");
       window.alert(message);
     },
-    token: "token",
+    token: user?.token,
   });
 
-  return <ApiContext.Provider value={{ apis }}>{children}</ApiContext.Provider>;
+  return <ApiContext.Provider value={{apis}}>{children}</ApiContext.Provider>;
 };
 
 export const useApis = () => {
@@ -23,9 +25,9 @@ export const useApis = () => {
 
 const GuestApiContext = createContext<any>(null);
 
-export const GuestApiContextProvider = ({ children }: any) => {
+export const GuestApiContextProvider = ({children}: any) => {
   const guestApis = createGuestApis();
-  return <GuestApiContext.Provider value={{ guestApis }}>{children}</GuestApiContext.Provider>;
+  return <GuestApiContext.Provider value={{guestApis}}>{children}</GuestApiContext.Provider>;
 };
 
 export const useGuestApis = () => {
