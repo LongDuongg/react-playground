cafd405fcd452c512276e558373a7cdae6d80ef6
LongDuongg <long2622001@gmail.com>
2025-06-22 18:23:59 +0700

    profile page: add layout

--- real-world-app/src/routes/profile/profile.tsx
+++ real-world-app/src/routes/profile/profile.tsx
@@ -6,6 +6,7 @@ import {useAuth} from "../../context/auth.js";
 import {useApis} from "../../context/apis.js";
 import {useQuery, useQueryClient} from "@tanstack/react-query";
 import {QUERY_KEYS} from "../../constant/query-keys.js";
+import {Layout} from "../layout/layout.js";
 
 export const Profile = () => {
   const queryClient = useQueryClient();
@@ -28,50 +29,52 @@ export const Profile = () => {
   }
 
   return (
-    <div className="profile-page">
-      <div className="user-info">
+    <Layout>
+      <div className="profile-page">
+        <div className="user-info">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12 col-md-10 offset-md-1">
+                <img src={profile.data.profile.image} className="user-img" />
+                {user.username === profile.data.profile.username ? (
+                  <>
+                    {" "}
+                    <h4>{user.username}</h4>
+                    <p>{user.bio}</p>
+                    <Link to={"/settings"}>
+                      <button className="btn btn-sm btn-outline-secondary action-btn">
+                        <i className="ion-gear-a"></i>
+                        &nbsp; Edit Profile Settings
+                      </button>
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    {" "}
+                    <h4>{profile.data.profile.username}</h4>
+                    <p>{profile.data.profile.bio}</p>
+                    <FollowButton
+                      userInfo={profile.data.profile}
+                      className={"action-btn"}
+                      onFollow={(updatedProfile) => {
+                        queryClient.setQueryData([QUERY_KEYS.auth.profile, params.username], {...profile.data, profile: updatedProfile});
+                      }}
+                    />
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="container">
           <div className="row">
             <div className="col-xs-12 col-md-10 offset-md-1">
-              <img src={profile.data.profile.image} className="user-img" />
-              {user.username === profile.data.profile.username ? (
-                <>
-                  {" "}
-                  <h4>{user.username}</h4>
-                  <p>{user.bio}</p>
-                  <Link to={"/settings"}>
-                    <button className="btn btn-sm btn-outline-secondary action-btn">
-                      <i className="ion-gear-a"></i>
-                      &nbsp; Edit Profile Settings
-                    </button>
-                  </Link>
-                </>
-              ) : (
-                <>
-                  {" "}
-                  <h4>{profile.data.profile.username}</h4>
-                  <p>{profile.data.profile.bio}</p>
-                  <FollowButton
-                    userInfo={profile.data.profile}
-                    className={"action-btn"}
-                    onFollow={(updatedProfile) => {
-                      queryClient.setQueryData([QUERY_KEYS.auth.profile, params.username], {...profile.data, profile: updatedProfile});
-                    }}
-                  />
-                </>
-              )}
+              <ArticleTabs profile={profile.data.profile} />
             </div>
           </div>
         </div>
       </div>
-
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-10 offset-md-1">
-            <ArticleTabs profile={profile.data.profile} />
-          </div>
-        </div>
-      </div>
-    </div>
+    </Layout>
   );
 };
